8895ada7a8889a95406f9eb1384d9387d4bad479
Luke Leber <lukeleber@gmail.com>
2017-01-06 09:25:08 -0500

    License Headers & Documentation

--- app/code/community/Stabilis/Core/Block/Adminhtml/Catalog/Category/Widget/Chooser.php
+++ app/code/community/Stabilis/Core/Block/Adminhtml/Catalog/Category/Widget/Chooser.php
@@ -1,5 +1,37 @@
 <?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Stabilis to newer
+ * versions in the future. If you wish to customize Stabilis for your
+ * needs please do so within the local code pool.
+ *
+ * @category    Stabilis
+ * @package     Stabilis_Core
+ * @copyright  Copyright (c) 2007-2016 Luke A. Leber (https://www.thinklikeamage.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
+/**
+ * A tab friendly category chooser that works with form html id prefixes 
+ * and suffixes.  This class also adds convenience dependency identifiers.
+ *
+ * @category    Stabilis
+ * @package     Stabilis_Core
+ * @author      Luke A. Leber <lukeleber@gmail.com>
+ */
 class Stabilis_Core_Block_Adminhtml_Catalog_Category_Widget_Chooser extends Mage_Adminhtml_Block_Catalog_Category_Widget_Chooser {
 
     /**
@@ -11,7 +43,6 @@ class Stabilis_Core_Block_Adminhtml_Catalog_Category_Widget_Chooser extends Mage
     public function prepareElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         $uniqId = Mage::helper('core')->uniqHash($element->getId());
-        
         $sourceUrl = $this->getUrl(
             '*/catalog_category_widget/chooser', 
             array(
--- app/code/community/Stabilis/Core/Block/Adminhtml/Widget/Chooser.php
+++ app/code/community/Stabilis/Core/Block/Adminhtml/Widget/Chooser.php
@@ -1,6 +1,38 @@
 <?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Stabilis to newer
+ * versions in the future. If you wish to customize Stabilis for your
+ * needs please do so within the local code pool.
+ *
+ * @category    Stabilis
+ * @package     Stabilis_Core
+ * @copyright  Copyright (c) 2007-2016 Luke A. Leber (https://www.thinklikeamage.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
+/**
+ * A tab friendly chooser widget that works with form html id prefixes and suffixes.
+ *
+ * @category    Stabilis
+ * @package     Stabilis_Core
+ * @author      Luke A. Leber <lukeleber@gmail.com>
+ */
 class Stabilis_Core_Block_Adminhtml_Widget_Chooser extends Mage_Widget_Block_Adminhtml_Widget_Chooser {
+    
     /**
      * Return chooser HTML and init scripts
      *
@@ -9,8 +41,11 @@ class Stabilis_Core_Block_Adminhtml_Widget_Chooser extends Mage_Widget_Block_Adm
     protected function _toHtml()
     {
         $element   = $this->getElement();
-        /* @var $fieldset Varien_Data_Form_Element_Fieldset */
+        /* @var $element Varien_Data_Form_Element_Abstract */
+        
         $fieldset  = $element->getForm()->getElement($this->getFieldsetId());
+        /* @var $fieldset Varien_Data_Form_Element_Fieldset */
+        
         $chooserId = $this->getUniqId();
         $config    = $this->getConfig();
 
